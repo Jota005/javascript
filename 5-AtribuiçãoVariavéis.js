8a4276e58db1df0648f966e25cafcd0c364d3b82
Jânisson Silva Melo <jota002silva9@gmail.com>
2024-06-13 11:02:21 -0300

    trabalhando com loops

---
+++ 5-AtribuiçãoVariavéis.js
@@ -0,0 +1,10 @@
+console.log("trabalhando com atribuição de variáveis"); 
+const nome = "Ricardo";
+const sobrenome ="Bugan";
+console.log(nome,  sobrenome)
+console.log(`Meu nome é ${nome} ${sobrenome}`)
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade+1;
+console.log(idade);
+
